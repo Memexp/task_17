cf9273718dd3dcf929925cd34f321593fc313d94
Maram Kamil <maram.kamil@gmail.com>
2019-02-20 13:03:32 +0300

    yay

--- api/views.py
+++ api/views.py
@@ -11,21 +11,30 @@ from .serializers import (
     RestaurantDetailSerializer,
     RestaurantCreateUpdateSerializer,
 )
+from rest_framework.permissions import (
+    AllowAny,
+    IsAdminUser,
+    IsAuthenticated,
+    )
+from .permissions import IsOwnerOrStaff
 
 class RestaurantListView(ListAPIView):
     queryset = Restaurant.objects.all()
     serializer_class = RestaurantListSerializer
+    permission_classes= [AllowAny,]
 
 
 class RestaurantDetailView(RetrieveAPIView):
     queryset = Restaurant.objects.all()
     serializer_class = RestaurantDetailSerializer
+    permission_classes= [AllowAny,]
     lookup_field = 'id'
     lookup_url_kwarg = 'restaurant_id'
 
 
 class RestaurantCreateView(CreateAPIView):
     serializer_class = RestaurantCreateUpdateSerializer
+    permission_classes= [IsAuthenticated,]
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
@@ -33,6 +42,7 @@ class RestaurantCreateView(CreateAPIView):
 class RestaurantUpdateView(RetrieveUpdateAPIView):
     queryset = Restaurant.objects.all()
     serializer_class = RestaurantCreateUpdateSerializer
+    permission_classes= [IsAuthenticated, IsOwnerOrStaff,]
     lookup_field = 'id'
     lookup_url_kwarg = 'restaurant_id'
 
@@ -40,5 +50,6 @@ class RestaurantUpdateView(RetrieveUpdateAPIView):
 class RestaurantDeleteView(DestroyAPIView):
     queryset = Restaurant.objects.all()
     serializer_class = RestaurantListSerializer
+    permission_classes= [IsAuthenticated, IsAdminUser,]
     lookup_field = 'id'
     lookup_url_kwarg = 'restaurant_id'
